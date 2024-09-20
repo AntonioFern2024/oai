d9309b7cb2b61e85bb2dfbc772e4b69c78dae083
Guido Casati <hello@guidocasati.com>
2024-09-20 16:22:06 +0200

    Explicit protocol_discriminator size in fgs_nas_message_security_header_t
    * size of fgs_nas_message_security_header_t is platform-depending
      while protocol_discriminator must be 1 byte

--- openair3/NAS/NR_UE/nr_nas_msg_sim.h
+++ openair3/NAS/NR_UE/nr_nas_msg_sim.h
@@ -125,10 +125,10 @@ typedef struct {
 
 /* Structure of security protected header */
 typedef struct {
-  fgs_protocol_discriminator_t    protocol_discriminator;
-  uint8_t                         security_header_type;
-  uint32_t                        message_authentication_code;
-  uint8_t                         sequence_number;
+  uint8_t protocol_discriminator;
+  uint8_t security_header_type;
+  uint32_t message_authentication_code;
+  uint8_t sequence_number;
 } fgs_nas_message_security_header_t;
 
 typedef union {
