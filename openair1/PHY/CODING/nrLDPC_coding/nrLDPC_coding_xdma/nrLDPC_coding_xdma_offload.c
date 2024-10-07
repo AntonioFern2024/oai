91abb41a318bd0dfed6a99debfbb5d2a2460dbf0
Romain Beurdouche <romain.beurdouche@eurecom.fr>
2024-10-07 14:52:55 +0200

    fix(nrLDPC_coding_interface): Select BSD license on XDMA wrapper files with Xilinx copyright

--- openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_xdma/nrLDPC_coding_xdma_offload.c
+++ openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_xdma/nrLDPC_coding_xdma_offload.c
@@ -1,13 +1,9 @@
 /*
- * This file is part of the Xilinx DMA IP Core driver tools for Linux
- *
  * Copyright (c) 2016-present,  Xilinx, Inc.
  * All rights reserved.
  *
- * This source code is licensed under both the BSD-style license (found in the
- * LICENSE file in the root directory of this source tree) and the GPLv2 (found
- * in the COPYING file in the root directory of this source tree).
- * You may select, at your option, one of the above-listed licenses.
+ * This source code is licensed under the BSD-style license (found in the
+ * LICENSE file in this directory).
  */
 
 #define _BSD_SOURCE
--- openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_xdma/xdma_diag.h
+++ openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_xdma/xdma_diag.h
@@ -1,14 +1,11 @@
 /*
- * This file is part of the Xilinx DMA IP Core driver tools for Linux
- *
  * Copyright (c) 2016-present,  Xilinx, Inc.
  * All rights reserved.
  *
- * This source code is licensed under both the BSD-style license (found in the
- * LICENSE file in the root directory of this source tree) and the GPLv2 (found
- * in the COPYING file in the root directory of this source tree).
- * You may select, at your option, one of the above-listed licenses.
+ * This source code is licensed under the BSD-style license (found in the
+ * LICENSE file in this directory).
  */
+
 #ifndef MODULES_TXCTRL_INC_XDMA_DIAG_H_
 #define MODULES_TXCTRL_INC_XDMA_DIAG_H_
 
