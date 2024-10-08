530ec7f97f2faa376f7e2e97ab61ae82b09cb3d4
Robert Schmidt <robert.schmidt@openairinterface.org>
2024-10-08 11:06:55 +0200

    Open TODOs at RRC

--- openair2/RRC/NR/rrc_gNB.c
+++ openair2/RRC/NR/rrc_gNB.c
@@ -2077,8 +2077,10 @@ static void rrc_CU_process_ue_context_release_request(MessageDef *msg_p, sctp_as
   f1ap_ue_context_release_req_t *req = &F1AP_UE_CONTEXT_RELEASE_REQ(msg_p);
   gNB_RRC_INST *rrc = RC.nrrrc[instance];
   rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context(rrc, req->gNB_CU_ue_id);
+  // TODO what happens if no AMF connected? should also handle, set an_release true
   if (!ue_context_p) {
     LOG_E(RRC, "could not find UE context for CU UE ID %u, aborting transaction\n", req->gNB_CU_ue_id);
+    // TODO just request the DU to release to make it happy
     return;
   }
 
@@ -2230,6 +2232,8 @@ static void rrc_CU_process_ue_modification_required(MessageDef *msg_p)
     rrc->mac_rrc.ue_context_modification_refuse(msg_p->ittiMsgHeader.originInstance, &refuse);
     return;
   }
+
+  // TODO ignore if handover ongoing
 
   if (required->du_to_cu_rrc_information && required->du_to_cu_rrc_information->cellGroupConfig) {
     gNB_RRC_UE_t *UE = &ue_context_p->ue_context;
